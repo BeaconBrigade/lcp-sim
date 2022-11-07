83c8a67826830cdf3a46912bb3f7e1fa0b0cb65f
BeaconBrigade <100320298+BeaconBrigade@users.noreply.github.com>
2022-11-07 07:20:13 -0330

    (WIP) chem-eq

---
+++ chem-eq/src/lib.rs
@@ -0,0 +1,85 @@
+//! # `chem-eq`
+//!
+//! `chem-eq` parses chemical equations into Elements and mol ratio
+//!
+//!
+
+use std::str::FromStr;
+
+use nom::error::Error;
+
+mod parse;
+
+/// A Chemical Equation. Containing a left and right side. Also keeps
+/// track of the mol ratio.
+///
+/// Eg: 4Fe + 3O2 -> 2Fe2O3
+#[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Equation {
+    pub left: Vec<Compound>,
+    pub right: Vec<Compound>,
+}
+
+/// An inidiviual compound. Containing some elements and a coefficient
+///
+/// Eg: 2Fe2O3
+#[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Compound {
+    pub elements: Vec<Element>,
+    pub coefficient: usize,
+    pub state: Option<State>,
+}
+
+/// An individual element. Containing an element from the periodic table
+/// and the count of how many there are.
+///
+/// Eg: O2
+#[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Element {
+    pub name: String,
+    pub count: usize,
+}
+
+/// The state of matter of a Compound. Including:
+/// - solid
+/// - liquid
+/// - gas
+/// - aqueous
+#[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum State {
+    Solid,
+    Liquid,
+    Gas,
+    #[default]
+    Aqueous,
+}
+
+impl FromStr for State {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "s" => Ok(Self::Solid),
+            "l" => Ok(Self::Liquid),
+            "g" => Ok(Self::Gas),
+            "aq" => Ok(Self::Aqueous),
+            _ => Err("Invalid state."),
+        }
+    }
+}
+
+impl Equation {
+    /// Create an [`Equation`] from a [`str`]. Fails if the string couldn't
+    /// be parsed.
+    pub fn new<'a, T: AsRef<&'a str>>(input: T) -> Result<Self, nom::Err<Error<&'a str>>> {
+        let (_, eq) = parse::parse_equation(input.as_ref())?;
+        Ok(eq)
+    }
+
+    /// Get the mol ration of the equation (left over right)
+    pub fn mol_ration(&self) -> f64 {
+        let left = self.left.iter().map(|c| c.coefficient).sum::<usize>();
+        let right = self.right.iter().map(|c| c.coefficient).sum::<usize>();
+        left as f64 / right as f64
+    }
+}
