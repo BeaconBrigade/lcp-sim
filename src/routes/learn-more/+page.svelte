a553be3bc9970125e54f661d280ed4652119f0ea
beaconbrigade <beaconbrigade@gmail.com>
2023-01-11 15:27:03 -0330

    (wip) writing learn more page

--- src/routes/learn-more/+page.svelte
+++ src/routes/learn-more/+page.svelte
@@ -1,5 +1,25 @@
-<script>
+<script lang="ts">
 	import Padded from '$lib/Padded.svelte';
+
+	let concentration: HTMLDivElement;
+	let temperature: HTMLDivElement;
+	let volume: HTMLDivElement;
+
+	function select(toggle: string) {
+		if (toggle === 'concentration') {
+			concentration.classList.toggle('shown');
+			temperature.classList.remove('shown');
+			volume.classList.remove('shown');
+		} else if (toggle === 'temperature') {
+			temperature.classList.toggle('shown');
+			volume.classList.remove('shown');
+			concentration.classList.remove('shown');
+		} else {
+			volume.classList.toggle('shown');
+			temperature.classList.remove('shown');
+			concentration.classList.remove('shown');
+		}
+	}
 </script>
 
 <div class="main">
@@ -7,6 +27,50 @@
 	<div class="main-menu">
 		<Padded href="/">Main Menu</Padded>
 	</div>
+
+	<p>
+		Le Chatelier's principle predicts the effect of a change in the conditions of an equilibrium.
+		According to the principle: when an equilibrium experiences a change in concentration,
+		temperature or volume it will shift to create a new equilibrium which counteracts the initial
+		change.
+	</p>
+
+	<p>
+		In an equilibrium system, there is an equation which can react in both directions. When the
+		system is at equilibrium the rate of forward reaction and rate of the reverse reaction are the
+		same, or the concentrations of the different compounds remains mostly constant. When a change
+		occurs, the system will modify its forward or reverse reaction rates to relieve the stress. See
+		examples below:
+	</p>
+
+	<button class="open" on:click={() => select('concentration')}>Concentration</button>
+	<div class="collapsible" bind:this={concentration}>
+		<span class="equation">N2 + O2 ↔ N2O2</span>
+		<p>
+			The system will shift to oppose changes in concentration. So, in the above system, if N2 was added
+			the eqauation's forward reaction rate would increase (N2 + O2 -> N2O2). The system tries to react
+			away the excess N2 consuming more O2 and producing more N2O2.
+		</p>
+	</div>
+
+	<button class="open" on:click={() => select('temperature')}>Temperature</button>
+	<div class="collapsible" bind:this={temperature}>
+		<p>temperature temperature temperature temperature temperature temperature</p>
+	</div>
+
+	<button class="open" on:click={() => select('volume')}>Volume</button>
+	<div class="collapsible" bind:this={volume}>
+		<p>volume volume volume volume volume volume volume volume volume volume</p>
+	</div>
+
+	<!--This is to avoid unused styles-->
+	<p class="shown" style="display: none;" />
+	<p>
+		Want more information? Visit <a
+			class="source"
+			href="https://en.wikipedia.org/wiki/Le_Chatelier%27s_principle">Wikipedia</a
+		>
+	</p>
 </div>
 
 <style>
@@ -14,11 +78,77 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		margin-left: 15px;
+		margin-right: 10px;
+	}
+
+	.main > p {
+		font-size: 1rem;
 	}
 
 	.main-menu {
 		position: absolute;
 		right: 10px;
 		top: 30px;
+	}
+
+	.source {
+		color: lightgray;
+		text-decoration: none;
+		font-weight: bold;
+	}
+
+	.open {
+		background-color: #313131;
+		color: white;
+		border: none;
+		padding: 10px;
+		padding-left: 2rem;
+		width: 100%;
+		font-size: 1rem;
+		overflow: hidden;
+		text-align: left;
+		margin-bottom: 10px;
+	}
+
+	.open:hover {
+		cursor: pointer;
+		background-color: #343434;
+	}
+
+	.collapsible {
+		max-height: 0px;
+		transition: max-height 1s ease-in-out;
+		display: none;
+	}
+
+	.shown {
+		display: flex;
+		flex-direction: column;
+
+		max-height: 15rem;
+		padding: 10px;
+		margin-bottom: 10px;
+		width: 95vw;
+
+		background-color: #414141;
+		font-size: 0.9rem;
+	}
+
+	.collapsible > p {
+		font-size: 1rem;
+		padding-left: 10px;
+	}
+
+	.equation {
+		text-align: center;
+		background-color: #7f7f7f;
+		padding: 10px;
+		font-weight: bold;
+		width: max-content;
+		margin: 0px auto;
+
+		border: 2px solid #525151;
+		border-radius: 0.75rem;
 	}
 </style>
