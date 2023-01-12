8971de61391e29865ed788ca99f6a12b9fa43b6f
BeaconBrigade <100320298+BeaconBrigade@users.noreply.github.com>
2023-01-12 01:04:10 -0330

    fmt

--- src-tauri/crates/chatelier/examples/serde.rs
+++ src-tauri/crates/chatelier/examples/serde.rs
@@ -3,9 +3,18 @@ use std::error::Error;
 use chatelier::Adjustment;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Adjust: {}", serde_json::to_string(&Adjustment::Concentration("N2", 2.0))?);
-    println!("Adjust: {}", serde_json::to_string(&Adjustment::Temperature(10.0))?);
-    println!("Adjust: {}", serde_json::to_string(&Adjustment::Volume(1.0))?);
+    println!(
+        "Adjust: {}",
+        serde_json::to_string(&Adjustment::Concentration("N2", 2.0))?
+    );
+    println!(
+        "Adjust: {}",
+        serde_json::to_string(&Adjustment::Temperature(10.0))?
+    );
+    println!(
+        "Adjust: {}",
+        serde_json::to_string(&Adjustment::Volume(1.0))?
+    );
 
     Ok(())
 }
